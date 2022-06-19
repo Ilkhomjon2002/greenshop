f1fff197dea83c42c9936579d4278362c879895a
Ilkhomjon <ilhommaxsudov3@gmail.com>
2022-06-19 21:26:48 +0900

    done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,73 @@
 import React from "react";
-
+import "./app.css";
 class TableRow extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { selected: props.row };
+	}
+	trigger(e) {
+		this.props.parentCallback(this.state.selected);
+	}
+
+	onChangeInput({ target: { value, name } }) {
+		this.setState({ selected: { ...this.state.selected, [name]: value } });
+	}
 	render() {
 		return (
-			<tr>
+			<tr className="table-data">
 				<td>{this.props.row.id}</td>
-				<td>{this.props.row.name}</td>
-				<td>{this.props.row.age}</td>
-				<td>{this.props.row.address}</td>
-				<td>{this.props.row.status}</td>
+				<td>
+					{this.props.selected ? (
+						<input
+							value={this.state.selected.name}
+							onChange={this.onChangeInput.bind(this)}
+							name="name"
+						></input>
+					) : (
+						this.props.row.name
+					)}
+				</td>
+				<td>
+					{this.props.selected ? (
+						<input
+							onChange={this.onChangeInput.bind(this)}
+							value={this.state.selected.age}
+							name="age"
+						></input>
+					) : (
+						this.props.row.age
+					)}
+				</td>
+				<td>
+					{this.props.selected ? (
+						<input
+							onChange={this.onChangeInput.bind(this)}
+							value={this.state.selected.address}
+							name="address"
+						></input>
+					) : (
+						this.props.row.address
+					)}
+				</td>
+				<td>
+					{this.props.selected ? (
+						<input
+							onChange={this.onChangeInput.bind(this)}
+							value={this.state.selected.status}
+							name="status"
+						></input>
+					) : (
+						this.props.row.status
+					)}
+				</td>
+				<td className="buttons">
+					{this.props.children}
+					{this.props.selected ? (
+						<button onClick={() => this.trigger()}>Save</button>
+					) : (
+						<button onClick={() => this.trigger()}>Edit</button>
+					)}
+				</td>
 			</tr>
 		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { TableRow } from "./App";
 import { data } from "./data";
+import { TableComponent } from "./table-component";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<div className="table-container">
-			<table>
-				<tr className="table-header">
-					<th>ID</th>
-					<th>Name</th>
-					<th>Age</th>
-					<th>Address</th>
-					<th>Status</th>
-				</tr>
-
-				{data.map((val) => {
-					return <TableRow row={val} />;
-				})}
-			</table>
-		</div>
+		<TableComponent data={data}></TableComponent>
+		{/* <Practise></Practise> */}
 	</React.StrictMode>
 );
